b5f25c8ace2365544df7908f145a807751c54a76
rohith-om <techaxis318@gmail.com>
2024-10-09 19:49:13 +0530

    workshop

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -27,13 +27,14 @@ async function main() {
     console.log("Failed to read contract files");
     process.exit(1);
   }
+  console.log("Declare & deploy contract. \n")
 
   const myCallData = new CallData(sierraCode.abi);
   const constructor = myCallData.compile("constructor", {
-    initial_value: 100,
-    kill_switch:
+    init_value: 100,
+    _kill_switch:
       "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
-    initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
+    _initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
   });
   const deployResponse = await account0.declareAndDeploy({
     contract: sierraCode,
@@ -48,9 +49,8 @@ async function main() {
     deployResponse.deploy.contract_address,
     provider
   );
-  console.log(
-    `✅ Contract has been deploy with the address: ${myTestContract.address}`
-  );
+  console.log(`✅ Contract has been deployed with the address: ${myTestContract.address}`);
+
 }
 main()
   .then(() => process.exit(0))
